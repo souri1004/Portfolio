0cb0e80159baef47869a7be0e594ffa69aa60544
souri1004 <sourav041005@gmail.com>
2025-07-21 02:34:16 +0530

    Deleted unused images and updated HTML/CSS

---
+++ index.js
@@ -0,0 +1,21 @@
+// let a= "sourav";
+// let b= 6;
+// console.log(a+b);
+// console.log(typeof (a+b));
+ 
+// const a={
+//     name: "sourav",
+//     age: 6,
+//     isActive: true,
+// }
+// a['friend']="shubham";
+// console.log(a);
+// a['name']="sourav kumar";
+// console.log(a);
+
+let a="sour\rav";
+let b="sour\nav";
+console.log(a);
+console.log(a.length);
+console.log(b);
+console.log(b.length);
